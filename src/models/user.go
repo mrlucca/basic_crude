ecde32ab1e6d075cbb5a4b5067003337e10a609c
lucca <vini.lucca42@gmail.com>
2021-03-25 18:08:16 -0300

    New fields for user model

--- src/models/user.go
+++ src/models/user.go
@@ -1,8 +1,10 @@
 package models
 
 type User struct {
+	Id       string `json:"id"`
 	Name     string `json:"name"`
 	Nickname string `json:"nickname"`
-	Age      int    `json:"age"`
+	Email    int    `json:"email"`
 	Password string `json:"password"`
+	Admin    bool   `json:"admin"`
 }
