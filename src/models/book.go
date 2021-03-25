8456c5c8c914cb362ee290f67a9ffa71bcd7a09a
lucca <vini.lucca42@gmail.com>
2021-03-25 17:04:53 -0300

    New field in book

--- src/models/book.go
+++ src/models/book.go
@@ -5,8 +5,10 @@ import (
 )
 
 type Book struct {
+	Id              string    `json:"id"`
 	Title           string    `json:"title"`
 	Subtitle        string    `json:"subtitle"`
+	Price           int       `json:"price"`
 	PublicationDate time.Time `json:"publication_date"`
 	Image           []byte    `json:"image"`
 }
