1b1b8a7118df69f2e1c6d121170fa4ab93151db8
lucca <vini.lucca42@gmail.com>
2021-03-25 20:40:17 -0300

    module env implemented
    This module created for basic configs in server

---
+++ env/db.go
@@ -0,0 +1,11 @@
+package env
+
+const (
+	DB_NAME     string = "lucca"
+	DB_USER     string = "lucca"
+	DB_PASSWORD string = "lucca"
+	DB_HOST     string = "localhost"
+	DB_PORT     string = "5432"
+)
+
+const Schema string = "basic_crud"
