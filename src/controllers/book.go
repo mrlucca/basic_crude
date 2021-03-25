06e03ed2b12688d26955db271b7349f4e18a21c3
lucca <vini.lucca42@gmail.com>
2021-03-25 17:04:39 -0300

    validation controllers

--- src/controllers/book.go
+++ src/controllers/book.go
@@ -11,10 +11,19 @@ import (
 )
 
 func Book(w http.ResponseWriter, r *http.Request) {
-
 	switch r.Method {
 	case "GET":
-		fmt.Fprintf(w, "Get all books")
+		geted, err := book.Get()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if geted {
+			fmt.Fprintf(w, "Book geted!")
+		} else {
+			http.Error(w, "Book not geted!", http.StatusNotFound)
+
+		}
 	case "POST":
 		var newBook models.Book
 		err := json.NewDecoder(r.Body).Decode(&newBook)
@@ -28,12 +37,32 @@ func Book(w http.ResponseWriter, r *http.Request) {
 		if created {
 			fmt.Fprintf(w, "Book created!")
 		} else {
-			fmt.Fprintf(w, "Book not created!")
+			http.Error(w, "Book not created!", http.StatusNotFound)
+
 		}
+
 	case "PUT":
-		fmt.Fprintf(w, "Change Book")
+		updated, err := book.Update()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if updated {
+			fmt.Fprintf(w, "Book updated")
+		} else {
+			http.Error(w, "Book not updated", http.StatusNotFound)
+		}
+
 	case "DELETE":
-		fmt.Fprintf(w, "Delete Book")
+		deleted, err := book.Delete()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if deleted {
+			fmt.Fprintf(w, "Book deleted!")
+		} else {
+			http.Error(w, "Book not deleted!", http.StatusNotFound)
+		}
+
 	default:
 		msg := fmt.Sprintf("Method %v not implemented", r.Method)
 		http.Error(w, msg, http.StatusNotFound)
